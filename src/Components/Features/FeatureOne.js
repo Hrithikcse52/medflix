7370b2ea2e80c7140f9e3f42c93e993c38efa1ed
Hrithikcse52 <hrithik.cse52@gmail.com>
2021-12-02 01:22:35 +0530

    home page syle changed

--- src/Components/Features/FeatureOne.js
+++ src/Components/Features/FeatureOne.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-    chakra,
-    Box,
-    Flex,
-    useColorModeValue,
-    Icon,
-    Stack,
-} from '@chakra-ui/react';
+import { chakra, Box, Flex, useColorModeValue, Icon, Stack } from '@chakra-ui/react';
 
 export default function FeatureOne() {
     const Feature = (props) => {
@@ -39,9 +32,7 @@ export default function FeatureOne() {
                         color={useColorModeValue('gray.900')}>
                         {props.title}
                     </chakra.dt>
-                    <chakra.dd
-                        mt={2}
-                        color={useColorModeValue('gray.500', 'gray.400')}>
+                    <chakra.dd mt={2} color={useColorModeValue('gray.500', 'gray.400')}>
                         {props.children}
                     </chakra.dd>
                 </Box>
@@ -51,14 +42,11 @@ export default function FeatureOne() {
     return (
         <Flex
             bg={useColorModeValue('#F9FAFB', 'gray.600')}
-            p={20}
+            p={10}
             w="auto"
             justifyContent="center"
             alignItems="center">
-            <Box
-                py={12}
-                bg={useColorModeValue('white', 'gray.800')}
-                rounded="xl">
+            <Box py={12} bg={useColorModeValue('white', 'gray.800')} rounded="xl">
                 <Box maxW="7xl" mx="auto" px={{ base: 4, lg: 8 }}>
                     <Box textAlign={{ lg: 'center' }}>
                         <chakra.h2
@@ -83,9 +71,8 @@ export default function FeatureOne() {
                             fontSize="xl"
                             mx={{ lg: 'auto' }}
                             color={useColorModeValue('gray.500', 'gray.400')}>
-                            Lorem ipsum dolor sit amet consect adipisicing elit.
-                            Possimus magnam voluptatum cupiditate veritatis in
-                            accusamus quisquam.
+                            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
+                            voluptatum cupiditate veritatis in accusamus quisquam.
                         </chakra.p>
                     </Box>
 
@@ -106,9 +93,8 @@ export default function FeatureOne() {
                                         d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
                                     />
                                 }>
-                                Lorem ipsum, dolor sit amet consectetur
-                                adipisicing elit. Maiores impedit perferendis
-                                suscipit eaque, iste dolor cupiditate blanditiis
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+                                impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis
                                 ratione.
                             </Feature>
 
@@ -122,9 +108,8 @@ export default function FeatureOne() {
                                         d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
                                     />
                                 }>
-                                Lorem ipsum, dolor sit amet consectetur
-                                adipisicing elit. Maiores impedit perferendis
-                                suscipit eaque, iste dolor cupiditate blanditiis
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+                                impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis
                                 ratione.
                             </Feature>
 
@@ -138,9 +123,8 @@ export default function FeatureOne() {
                                         d="M13 10V3L4 14h7v7l9-11h-7z"
                                     />
                                 }>
-                                Lorem ipsum, dolor sit amet consectetur
-                                adipisicing elit. Maiores impedit perferendis
-                                suscipit eaque, iste dolor cupiditate blanditiis
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+                                impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis
                                 ratione.
                             </Feature>
 
@@ -154,9 +138,8 @@ export default function FeatureOne() {
                                         d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
                                     />
                                 }>
-                                Lorem ipsum, dolor sit amet consectetur
-                                adipisicing elit. Maiores impedit perferendis
-                                suscipit eaque, iste dolor cupiditate blanditiis
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+                                impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis
                                 ratione.
                             </Feature>
                         </Stack>
